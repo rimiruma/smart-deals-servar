66e5c14d4d6ccf6b3bf920eb19d4c03923562095
rimiruma <rimiruma12@gmail.com>
2025-11-03 09:35:24 +0600

    server bids api done

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
       const query = { email: email }
       const existingUser = await usersCollection.findOne(query)
       if (existingUser) {
-        res.send({message: 'user already exits.'})
+        res.send({ message: 'user already exits.' })
       }
       else {
         const result = await usersCollection.insertOne(newUser);
@@ -68,8 +68,8 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/latest-products', async(req, res) => {
-      const cursor = productsCollection.find().sort({created_at: -1}).limit(6);
+    app.get('/latest-products', async (req, res) => {
+      const cursor = productsCollection.find().sort({ created_at: -1 }).limit(6);
       const result = await cursor.toArray();
       res.send(result);
     })
@@ -118,6 +118,14 @@ async function run() {
       const cursor = bidsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    app.post('/product/bids/:productId', async (req, res) => {
+      const productId = req.params.productId;
+      const query = {product: productId}
+      const cursor = bidsCollection.find(query).sort({bid_price: -1})
+      const result = await cursor.toArray();
+      res.send(result)
     })
 
     app.post('/bids', async (req, res) => {
